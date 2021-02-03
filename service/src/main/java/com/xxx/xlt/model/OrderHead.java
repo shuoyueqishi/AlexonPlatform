421d26571bbd5d6baf3929d7eb7e26a1fc93001f
xlt <2219321592@qq.com>
2021-02-03 23:47:07 +0800

    excel导出demo

--- service/src/main/java/com/xxx/xlt/model/OrderHead.java
+++ service/src/main/java/com/xxx/xlt/model/OrderHead.java
@@ -1,6 +1,7 @@
 package com.xxx.xlt.model;
 
 import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.write.style.ColumnWidth;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,10 +15,16 @@ import java.io.Serializable;
 @AllArgsConstructor
 public class OrderHead implements Serializable {
     private static final long serialVersionUID = -4616429795083062747L;
-    @ExcelProperty("订单ID")
+
+    @ExcelProperty(value="订单ID", index = 0)
+    @ColumnWidth(15)
     private Long orderHeadId;
-    @ExcelProperty("下单日期")
+
+    @ExcelProperty(value="下单日期", index = 2)
+    @ColumnWidth(20)
     private String orderDate;
-    @ExcelProperty("订单号")
+
+    @ExcelProperty(value="订单号", index = 1)
+    @ColumnWidth(10)
     private String orderNo;
 }
